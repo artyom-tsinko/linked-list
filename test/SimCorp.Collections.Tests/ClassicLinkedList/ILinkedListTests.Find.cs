eb061d6737fe5657b2c0599720b9d432322390b2
Artyom Tsinko <atsinko@smartspaceplc.com>
2020-08-25 16:07:22 +0300

    ToArray and Find test coverage

--- test/SimCorp.Collections.Tests/ClassicLinkedList/ILinkedListTests.Find.cs
+++ test/SimCorp.Collections.Tests/ClassicLinkedList/ILinkedListTests.Find.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 using SimCorp.Collections.ClassicLinkedList;
 
@@ -10,23 +8,112 @@ namespace SimCorp.Collections.Tests.ClassicLinkedList
     public static partial class ILinkedListTests
     {
 
-        [TestCaseSource(nameof(LinkedListImplemetations), new object[] { nameof(Find_Condition_ShouldResult) })]
-        public static void Find_Condition_ShouldResult(object list, bool testGenericInterface = false)
+        [TestCaseSource(nameof(LinkedListImplemetations), new object[] { nameof(Find_EmptyList_ShouldReturnNull) })]
+        public static void Find_EmptyList_ShouldReturnNull(object list, bool testGenericInterface = false)
         {
             list.ApplyTestCase(
                 testGenericInterface,
-                l => Find_Condition_ShouldResult_Test<ISinglyLinkedListNode>(l),
-                l => Find_Condition_ShouldResult_Test<IDoublyLinkedListNode>(l),
-                l => Find_Condition_ShouldResult_Test<ILinkedListNode>(l));
+                l => Find_EmptyList_ShouldReturnNull_Test<ISinglyLinkedListNode>(l),
+                l => Find_EmptyList_ShouldReturnNull_Test<IDoublyLinkedListNode>(l),
+                l => Find_EmptyList_ShouldReturnNull_Test<ILinkedListNode>(l));
 
         }
 
-        public static void Find_Condition_ShouldResult_Test<TNode>(ILinkedList<TNode> list)
+
+        public static void Find_EmptyList_ShouldReturnNull_Test<TNode>(ILinkedList<TNode> list)
             where TNode : class, ILinkedListNode
         {
             Assert.NotNull(list);
+
+            var node = list.Find("Jack Campbell");
+
+            Assert.IsNull(node);
         }
-        
+
+
+        [TestCaseSource(nameof(LinkedListImplemetations), new object[] { nameof(Find_FilledList_NoMatchingValue_ShouldReturnNull) })]
+        public static void Find_FilledList_NoMatchingValue_ShouldReturnNull(object list, bool testGenericInterface = false)
+        {
+            list.ApplyTestCase(
+                testGenericInterface,
+                l => Find_FilledList_NoMatchingValue_ShouldReturnNull_Test<ISinglyLinkedListNode>(l),
+                l => Find_FilledList_NoMatchingValue_ShouldReturnNull_Test<IDoublyLinkedListNode>(l),
+                l => Find_FilledList_NoMatchingValue_ShouldReturnNull_Test<ILinkedListNode>(l));
+
+        }
+
+
+        public static void Find_FilledList_NoMatchingValue_ShouldReturnNull_Test<TNode>(ILinkedList<TNode> list)
+            where TNode : class, ILinkedListNode
+        {
+            Assert.NotNull(list);
+
+            list.Add("Josephus Flavius");
+            list.Add("Herodotus");
+            list.Add("Tacitus");
+            
+            var node = list.Find("Alexander Argead");
+
+            Assert.IsNull(node);
+        }
+
+
+        [TestCaseSource(nameof(LinkedListImplemetations), new object[] { nameof(Find_FilledList_ContainsMatchingValue_ShouldReturnNode) })]
+        public static void Find_FilledList_ContainsMatchingValue_ShouldReturnNode(object list, bool testGenericInterface = false)
+        {
+            list.ApplyTestCase(
+                testGenericInterface,
+                l => Find_FilledList_ContainsMatchingValue_ShouldReturnNode_Test<ISinglyLinkedListNode>(l),
+                l => Find_FilledList_ContainsMatchingValue_ShouldReturnNode_Test<IDoublyLinkedListNode>(l),
+                l => Find_FilledList_ContainsMatchingValue_ShouldReturnNode_Test<ILinkedListNode>(l));
+
+        }
+
+
+        public static void Find_FilledList_ContainsMatchingValue_ShouldReturnNode_Test<TNode>(ILinkedList<TNode> list)
+            where TNode : class, ILinkedListNode
+        {
+            Assert.NotNull(list);
+
+            list.Add("Josephus Flavius");
+            var targetNode = list.Add("Herodotus");
+            list.Add("Tacitus");
+
+            var foundNode = list.Find("Herodotus");
+
+            Assert.AreSame(targetNode, foundNode);
+        }
+
+
+        [TestCaseSource(nameof(LinkedListImplemetations), new object[] { nameof(Find_FilledList_ContainsMultipleMatchingValues_ShouldReturnFirstMatchingNode) })]
+        public static void Find_FilledList_ContainsMultipleMatchingValues_ShouldReturnFirstMatchingNode(object list, bool testGenericInterface = false)
+        {
+            list.ApplyTestCase(
+                testGenericInterface,
+                l => Find_FilledList_ContainsMultipleMatchingValues_ShouldReturnFirstMatchingNode_Test<ISinglyLinkedListNode>(l),
+                l => Find_FilledList_ContainsMultipleMatchingValues_ShouldReturnFirstMatchingNode_Test<IDoublyLinkedListNode>(l),
+                l => Find_FilledList_ContainsMultipleMatchingValues_ShouldReturnFirstMatchingNode_Test<ILinkedListNode>(l));
+
+        }
+
+
+        public static void Find_FilledList_ContainsMultipleMatchingValues_ShouldReturnFirstMatchingNode_Test<TNode>(ILinkedList<TNode> list)
+            where TNode : class, ILinkedListNode
+        {
+            Assert.NotNull(list);
+
+            list.Add("Josephus Flavius");
+            var targetNode = list.Add("Herodotus");
+            list.Add("Herodotus");
+            list.Add("Herodotus");
+            list.Add("Herodotus");
+            list.Add("Tacitus");
+
+            var foundNode = list.Find("Herodotus");
+
+            Assert.AreSame(targetNode, foundNode);
+        }
+
     }
 
 }
--- test/SimCorp.Collections.Tests/ClassicLinkedList/ILinkedListTests.ToArray.cs
+++ test/SimCorp.Collections.Tests/ClassicLinkedList/ILinkedListTests.ToArray.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 
 using NUnit.Framework;
 
@@ -10,23 +10,58 @@ namespace SimCorp.Collections.Tests.ClassicLinkedList
     public static partial class ILinkedListTests
     {
 
-        [TestCaseSource(nameof(LinkedListImplemetations), new object[] { nameof(ToArray_Condition_ShouldResult) })]
-        public static void ToArray_Condition_ShouldResult(object list, bool testGenericInterface = false)
+        [TestCaseSource(nameof(LinkedListImplemetations), new object[] { nameof(ToArray_EmptyList_ShouldReturnEmptyArray) })]
+        public static void ToArray_EmptyList_ShouldReturnEmptyArray(object list, bool testGenericInterface = false)
         {
             list.ApplyTestCase(
                 testGenericInterface,
-                l => ToArray_Condition_ShouldResult_Test<ISinglyLinkedListNode>(l),
-                l => ToArray_Condition_ShouldResult_Test<IDoublyLinkedListNode>(l),
-                l => ToArray_Condition_ShouldResult_Test<ILinkedListNode>(l));
+                l => ToArray_EmptyList_ShouldReturnEmptyArray_Test<ISinglyLinkedListNode>(l),
+                l => ToArray_EmptyList_ShouldReturnEmptyArray_Test<IDoublyLinkedListNode>(l),
+                l => ToArray_EmptyList_ShouldReturnEmptyArray_Test<ILinkedListNode>(l));
 
         }
 
-        public static void ToArray_Condition_ShouldResult_Test<TNode>(ILinkedList<TNode> list)
+        public static void ToArray_EmptyList_ShouldReturnEmptyArray_Test<TNode>(ILinkedList<TNode> list)
             where TNode : class, ILinkedListNode
         {
             Assert.NotNull(list);
+
+            CollectionAssert.AreEqual(Enumerable.Empty<string>(), list.ToArray());
         }
-        
+
+
+        [TestCaseSource(nameof(LinkedListImplemetations), new object[] { nameof(ToArray_FilledList_ShouldReturnArrayItemsInListOrder) })]
+        public static void ToArray_FilledList_ShouldReturnArrayItemsInListOrder(object list, bool testGenericInterface = false)
+        {
+            list.ApplyTestCase(
+                testGenericInterface,
+                l => ToArray_FilledList_ShouldReturnArrayItemsInListOrder_Test<ISinglyLinkedListNode>(l),
+                l => ToArray_FilledList_ShouldReturnArrayItemsInListOrder_Test<IDoublyLinkedListNode>(l),
+                l => ToArray_FilledList_ShouldReturnArrayItemsInListOrder_Test<ILinkedListNode>(l));
+
+        }
+
+        public static void ToArray_FilledList_ShouldReturnArrayItemsInListOrder_Test<TNode>(ILinkedList<TNode> list)
+            where TNode : class, ILinkedListNode
+        {
+            Assert.NotNull(list);
+
+            const string nodeValue = "Roger Zelazny";
+            var counter = 0;
+
+            var node1 = list.Add($"{nodeValue}{++counter}");
+            var node2 = list.Add($"{nodeValue}{++counter}");
+            list.Add($"{nodeValue}{++counter}");
+            var node4 = list.Add($"{nodeValue}{++counter}");
+            list.Add($"{nodeValue}{++counter}");
+
+            list.Remove(node2);
+            list.Remove(node1);
+            list.Remove(node4);
+
+            CollectionAssert.AreEqual(new string[] { $"{nodeValue}3", $"{nodeValue}5" }, list.ToArray());
+        }
+
     }
 
 }
